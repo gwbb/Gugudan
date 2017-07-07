66aeb44505697c3e1052f3bfb00ad783798f3284
HHO <HHO@DESKTOP-GN6GCE2>
2017-07-07 17:32:09 +0900

    Four time Table
    use variable(result) and output variable

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,4 +1,50 @@
 
 public class Gugudan {
-
+	public static void main(String[] args) {
+		
+		// 2´Ü
+		System.out.println("Two time table");
+		System.out.println(2 * 1);
+		System.out.println(2 * 2);
+		System.out.println(2 * 3);
+		System.out.println(2 * 4);
+		System.out.println(2 * 5);
+		System.out.println(2 * 6);
+		System.out.println(2 * 7);
+		System.out.println(2 * 8);
+		System.out.println(2 * 9);
+		
+		// 3´Ü
+		System.out.println("Three time table");
+		System.out.println(3 * 1);
+		System.out.println(3 * 2);
+		System.out.println(3 * 3);
+		System.out.println(3 * 4);
+		System.out.println(3 * 5);
+		System.out.println(3 * 6);
+		System.out.println(3 * 7);
+		System.out.println(3 * 8);
+		System.out.println(3 * 9);
+	
+		// 4´Ü
+		System.out.println("Four time table");
+		int result = 4 * 1;
+		System.out.println(result);
+		result = 4 * 2;
+		System.out.println(result);
+		result = 4 * 3;
+		System.out.println(result);
+		result = 4 * 4;
+		System.out.println(result);
+		result = 4 * 5;
+		System.out.println(result);
+		result = 4 * 6;
+		System.out.println(result);
+		result = 4 * 7;
+		System.out.println(result);
+		result = 4 * 8;
+		System.out.println(result);
+		result = 4 * 9;
+		System.out.println(result);
+	}
 }

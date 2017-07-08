9688e4ce11a107da1bcf1736166db724fe9fe349
HHO <HHO@DESKTOP-GN6GCE2>
2017-07-08 17:19:40 +0900

    add scanner method
    selectable number

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class Gugudan {
 	public static void main(String[] args) {
+		System.out.println("Enter the number of time table you want : ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		System.out.println("User input number : " + number);
+		
 		int[] result = new int[9];
 		for(int i = 0; i < result.length; i++) {
-			result[i] = 2 * (i + 1);
+			result[i] = number * (i + 1);
 		}
 		
 		for(int i = 0; i < result.length; i++) {

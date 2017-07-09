7b4c736f98099cebc0f1217c4bb2639126b10e55
HHO <HHO@DESKTOP-GN6GCE2>
2017-07-09 15:28:49 +0900

    add method
    calculate, print

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,19 +1,22 @@
-import java.util.Scanner;
-
 public class Gugudan {
-	public static void main(String[] args) {
-		System.out.println("Enter the number of time table you want : ");
-		Scanner scanner = new Scanner(System.in);
-		int number = scanner.nextInt();
-		System.out.println("User input number : " + number);
-		
+	public static int[] calculate(int times) {
 		int[] result = new int[9];
 		for(int i = 0; i < result.length; i++) {
-			result[i] = number * (i + 1);
+			result[i] = times * (i + 1);
 		}
-		
+		return result;
+	}
+	
+	public static void print(int[] result) {
 		for(int i = 0; i < result.length; i++) {
 			System.out.println(result[i]);
+		}
+	}
+	
+	public static void main(String[] args) {
+		for (int i = 2; i < 10; i++) {
+			int[] result = calculate(i);
+			print(result);
 		}
 	}
 }

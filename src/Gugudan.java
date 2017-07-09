24a98ac28b50ea9248bc321f31f617523f58ef05
HHO <HHO@DESKTOP-GN6GCE2>
2017-07-09 17:59:57 +0900

    final mission (1/2) of codesquad
    User input a number
    
    calculate time table(input number * 1 ~ number * number), and print
    result in order

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,28 +1,27 @@
 import java.util.Scanner;
 
 public class Gugudan {
-	public static int[] calculate(int times) {
-		int[] result = new int[9];
-		for (int i = 0; i < result.length; i++) {
-			result[i] = times * (i + 1); 
-		}
+	public static int takenum() {
+		System.out.println("Enter the number of time table you want : ");
+		Scanner scanner = new Scanner(System.in);
+		int inputValue = scanner.nextInt();
+		
+		return inputValue;
+	}
+	
+	public static int[] calculate(int number) {
+		int[] result = new int[number];
+		for(int i = 0; i < number; i++) {
+			result[i] = number * (i + 1);
+	}
 		
 		return result;
 	}
 	
 	public static void print(int[] result) {
 		for(int i = 0; i < result.length; i++) {
-			System.out.println(result[i]);
+		System.out.println(result[i]);
 		}
-	}
-	
-	public static int takenum() {
-		System.out.println("Enter the number of time table you want : ");
-		Scanner scanner = new Scanner(System.in);
-		int number = scanner.nextInt();
 		
-		return number;
 	}
-	
-	
 }
--- src/GugudanMain.java
+++ src/GugudanMain.java
@@ -1,7 +1,7 @@
 public class GugudanMain {
 	public static void main(String[] args) {
-		int times = Gugudan.takenum();
-		int[] result = Gugudan.calculate(times);
+		int inputnum = Gugudan.takenum();
+		int[] result = Gugudan.calculate(inputnum);
 		Gugudan.print(result);
 	}
 }
